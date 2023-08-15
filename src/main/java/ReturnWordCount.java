0b326e6fc41c8a337a1d46b45bf44b39eb86cee0
Hoa Do <hoa.do.ma@gmail.com>
2023-08-15 03:52:37 +0000

    Completed Challenge: Word Count

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,8 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+
+        String[] stringArray = in.split(" ");
+        return stringArray.length;
     }
 }
